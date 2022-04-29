4d52f4e700f9c43a820fb128b2a4ef129284bba6
‘AW0005’ <bunnariums@gmail.com>
2022-04-29 17:19:09 -0400

    an attempt

--- config/mail.php
+++ config/mail.php
@@ -18,6 +18,31 @@ return [
 
     'driver' => env('MAIL_DRIVER', 'smtp'),
 
+    'mailers' => [
+        'mailjet' => [
+            'key' => env('MAILJET_APIKEY'),
+            'secret' => env('MAILJET_APISECRET'),
+            'transactional' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3.1',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ],
+            'common' => [
+                'call' => true,
+                'options' => [
+                    'url' => 'api.mailjet.com',
+                    'version' => 'v3',
+                    'call' => true,
+                    'secured' => true
+                ]
+            ]
+        ]
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | SMTP Host Address
