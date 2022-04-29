0dd5ce2a4548d3302766c0501f1adb3a0b296eb3
‘AW0005’ <bunnariums@gmail.com>
2022-04-29 18:43:35 -0400

    couple more things

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Mailjet\LaravelMailjet\MailjetServiceProvider::class,
+        Mailjet\LaravelMailjet\MailjetMailServiceProvider::class,
 
         /*
          * Package Service Providers...
