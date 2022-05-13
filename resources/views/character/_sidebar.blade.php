fcd0bb9a28f00b64624977bf4da61dade64f3389
‘AW0005’ <bunnariums@gmail.com>
2022-05-13 19:09:21 -0400

    Hide Character Banks and Inventories

--- resources/views/character/_sidebar.blade.php
+++ resources/views/character/_sidebar.blade.php
@@ -5,8 +5,8 @@
         <div class="sidebar-item"><a href="{{ $character->url }}" class="{{ set_active('character/'.$character->slug) }}">Information</a></div>
         <div class="sidebar-item"><a href="{{ $character->url . '/profile' }}" class="{{ set_active('character/'.$character->slug.'/profile') }}">Profile</a></div>
         <!--Removing Galleries:  <div class="sidebar-item"><a href="{{ $character->url . '/gallery' }}" class="{{ set_active('character/'.$character->slug.'/gallery') }}">Gallery</a></div> -->
-        <div class="sidebar-item"><a href="{{ $character->url . '/inventory' }}" class="{{ set_active('character/'.$character->slug.'/inventory') }}">Inventory</a></div>
-        <div class="sidebar-item"><a href="{{ $character->url . '/bank' }}" class="{{ set_active('character/'.$character->slug.'/bank') }}">Bank</a></div>
+        <!-- <div class="sidebar-item"><a href="{{ $character->url . '/inventory' }}" class="{{ set_active('character/'.$character->slug.'/inventory') }}">Inventory</a></div> -->
+        <!-- <div class="sidebar-item"><a href="{{ $character->url . '/bank' }}" class="{{ set_active('character/'.$character->slug.'/bank') }}">Bank</a></div> -->
     </li>
     <li class="sidebar-section">
         <div class="sidebar-section-header">History</div>
