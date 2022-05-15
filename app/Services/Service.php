72c00fc7d8552e9574b63c18e05bd6cd237c27e8
‘AW0005’ <bunnariums@gmail.com>
2022-05-15 15:03:21 -0400

    only other thing

--- app/Services/Service.php
+++ app/Services/Service.php
@@ -226,7 +226,7 @@ abstract class Service {
             chmod($dir, 0755);
         }
 
-        dd($copy);
+        dd($dir . '/' . $name);
         if($copy) File::copy($image, $dir . '/' . $name);
         else File::move($image, $dir . '/' . $name);
         chmod($dir . '/' . $name, 0755);
