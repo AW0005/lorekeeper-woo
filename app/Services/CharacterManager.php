e60f60f8bb6120291ba627fb59151c665f099ce7
‘AW0005’ <bunnariums@gmail.com>
2022-05-19 19:44:00 -0400

    fix

--- app/Services/CharacterManager.php
+++ app/Services/CharacterManager.php
@@ -355,9 +355,8 @@ class CharacterManager extends Service
             // Save image
             $this->handleImage($data['image'], $image->imageDirectory, $image->imageFileName, null, isset($data['default_image']));
 
-
             // Save thumbnail first before processing full image
-            if(isset($data['use_cropper'])) $this->cropThumbnail(Arr::only($data, ['x0','x1','y0','y1']), $image, $isMyo);
+            if(isset($imageData['use_cropper'])) $this->cropThumbnail(Arr::only($data, ['x0','x1','y0','y1']), $image, $isMyo);
             else $this->handleImage($data['thumbnail'], $image->imageDirectory, $image->thumbnailFileName, null, isset($data['default_image']));
 
             // Process and save the image itself
