6a66859590030ba6ec113b3179dbb1ef9679ce5b
‘AW0005’ <bunnariums@gmail.com>
2022-06-04 20:14:54 -0400

    Keep image small

--- resources/views/world/_feature_entry.blade.php
+++ resources/views/world/_feature_entry.blade.php
@@ -1,8 +1,8 @@
 <div class="row world-entry">
     @if($feature->has_image)
-        <div class="col-md-3 world-entry-image"><a href="{{ $feature->imageUrl }}" data-lightbox="entry" data-title="{{ $feature->name }}"><img src="{{ $feature->imageUrl }}" class="world-entry-image" alt="{{ $feature->name }}" /></a></div>
+        <div class="col-3 world-entry-image"><a href="{{ $feature->imageUrl }}" data-lightbox="entry" data-title="{{ $feature->name }}"><img src="{{ $feature->imageUrl }}" class="world-entry-image" alt="{{ $feature->name }}" /></a></div>
     @endif
-    <div class="{{ $feature->has_image ? 'col-md-9' : 'col-12' }}">
+    <div class="{{ $feature->has_image ? 'col-9' : 'col-12' }}">
         <h3>{!! $feature->displayName !!} <a href="{{ $feature->searchUrl }}" class="world-entry-search text-muted"><i class="fas fa-search"></i></a></h3>
         @if($feature->feature_category_id)
             <div><strong>Category:</strong> {!! $feature->category->displayName !!}</div>
