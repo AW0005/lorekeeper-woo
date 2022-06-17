c62f9c3ced2f5b424d86b2a83f716ddc7c931012
‘AW0005’ <bunnariums@gmail.com>
2022-06-17 11:40:52 -0400

    Logs: Styling updates

--- resources/views/admin/logs.blade.php
+++ resources/views/admin/logs.blade.php
@@ -2,31 +2,49 @@
 
 @section('admin-title') Log of Logs @endsection
 
+@section('scripts')
+<style>
+table tr td { border: none !important; }
+table tr { border-bottom: 1px solid #ccc }
+
+tr > td:first-child,
+tr > td:last-child {
+    white-space: nowrap;
+}
+</style>
+@endsection
+
 @section('admin-content')
 {!! breadcrumbs(['Admin Panel' => 'admin', 'Log of Logs' => 'logs']) !!}
 
 <h1>Log of Logs</h1>
 {!! $logs->render() !!}
-<div class="row ml-md-2 mb-4">
-  <div class="d-flex row flex-wrap col-12 mt-1 pt-1 px-0 ubt-bottom">
-    <div class="col-6 col-md-2 font-weight-bold">Log Date</div>
-    <div class="col-6 col-md-2 font-weight-bold">Sender</div>
-    <div class="col-6 col-md-2 font-weight-bold">Recipient</div>
-    <div class="col-6 col-md-4 font-weight-bold">Log</div>
-    <div class="col-6 col-md-2 font-weight-bold">Character</div>
-  </div>
+<table class="mb-2">
+    <thead>
+        <tr>
+            <th>Log Date</th>
+            <th>Sender</th>
+            <th></th>
+            <th>Recipient</th>
+            <th>Log</th>
+            <th>Character</th>
+        </tr>
+    </thead>
+    <tbody>
     @foreach($logs as $log)
-        <div class="d-flex row flex-wrap col-12 mt-1 pt-1 px-0 ubt-top">
-        <div class="col-6 col-md-2">{!! pretty_date($log->created_at) !!}</div>
-        <div class="col-6 col-md-2">
-            {!! $log->user ? $log->user->displayName : '' !!}
-            {!! $log->sender ? $log->sender->displayName.' <i style="color: red;" class="fas fa-minus-circle"></i>' : '' !!}
-        </div>
-        <div class="col-6 col-md-2">{!! $log->recipient ? $log->recipient->displayName.' <i style="color: green;" class="fas fa-plus-circle"></i>' : '' !!}</div>
-        <div class="col-6 col-md-4">{!! $log->log ? $log->log : $log->type !!}</div>
-        <div class="col-6 col-md-2">{!! $log->character ? $log->character->displayName : 'n/a' !!}</div>
-        </div>
+        <tr>
+            <td>{!! pretty_date($log->created_at) !!}</td>
+            <td>
+                {!! $log->user ? $log->user->displayName : '' !!}
+                {!! $log->sender ? $log->sender->displayName : '' !!}
+            </td>
+            <td><i class="fas fa-long-arrow-alt-right"></i></td>
+            <td>{!! $log->recipient ? $log->recipient->displayName : '' !!}</td>
+            <td>{!! $log->log ? $log->log : $log->type !!}</td>
+            <td>{!! $log->character ? $log->character->displayName : 'n/a' !!}</td>
+        </tr>
     @endforeach
-    {!! $logs->render() !!}
-</div>
+    </tbody>
+</table>
+{!! $logs->render() !!}
 @endsection
